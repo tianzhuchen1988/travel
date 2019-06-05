ac63c52d8dc4b7dc39d6efac6af85f9561a02c30
chentz <3690560@qq.com>
2019-06-05 17:59:22 +0800

    首页轮播图

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper :list="swiperList"></home-swiper>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
+  },
+  data () {
+    return {
+      swiperList: [{id: 1, imgUrl: '123'}]
+    }
   }
 }
 </script>
